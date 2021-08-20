07f808b4c71580d121f65f221e53ace216dd81cd
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-08-20 15:22:07 -0300

    urllib e getpass

---
+++ Códigos Python/credencias_getpass.py
@@ -0,0 +1,5 @@
+import getpass
+
+nome =  input("Digite o seu usuário: ")
+senha = getpass.getpass("Digite sua senha: ")
+print(senha)
--- Códigos Python/my_selenium.py
+++ Códigos Python/my_selenium.py
@@ -12,13 +12,18 @@ import time
 
 
 driver = webdriver.Chrome(executable_path=r'C:/chromedriver/chromedriver.exe')
-driver.get("http://www.google.com")
-# assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-#elem = driver.find_element_by_name("q").send_keys("pyplan" + Keys.RETURN)
-# elem.clear()
-elem.send_keys("pyplan")
+driver.get("https://e-decision.wca-ec.com.br/login.aspx")
+elem = driver.find_element_by_name("FormLayout$txtLogin")
+elem.send_keys("alexsandro.ignacio@wca-ec.com.br")
+elem = driver.find_element_by_name("FormLayout$txtPassword")
+elem.send_keys("wca2021@")
+elem = driver.find_element_by_xpath("FormLayout$txtPassword")
 elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
+
 time.sleep(1)
 driver.close()
+
+# assert "Python" in driver.title
+#elem = driver.find_element_by_name("q").send_keys("pyplan" + Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# elem.clear()
