fd2d43c17947fa8b0edad32fb542d7657dfd5db2
alexaugusto23 <contato.alexaugusto@hotmail.com>
2022-03-09 18:51:26 -0300

    json moinho 2

--- Planejamento - Lógicas/Json Examples by Project/Java Script Functions/function.js
+++ Planejamento - Lógicas/Json Examples by Project/Java Script Functions/function.js
@@ -22,9 +22,7 @@ let funcA = function(hotInstance, TD, row, col, prop, value, cellProperties)
 }
 
 
-
-
-
+// Moinhos Old
 function funcB(hotInstance, TD, row, col, prop, value, cellProperties) 
 {   const dataRow = hotInstance.getSourceDataAtRow(row); 
     const rel = dataRow.relatorio; 
@@ -54,6 +52,22 @@ function funcB(hotInstance, TD, row, col, prop, value, cellProperties)
     } 
 }
 
+// Moinhos new
+function funcc(hotInstance, TD, row, col, prop, value, cellProperties) 
+{   const dataRow = hotInstance.getSourceDataAtRow(row); 
+    const rel = dataRow.relatorio; 
+    if (dataRow.id >= 0) { cellProperties.readOnly = false; } 
+    if (rel == 'Marchas') { TD.style.background = '#F3F3F3'; TD.style.fontWeight = 'bold'; } 
+    if (rel == 'Horarios de Ponta') {
+        if (value == 0){TD.style.background = '#EAFB15'; TD.style.color = '#EAFB15';}
+        else{TD.style.background = '#F3F3F3';}
+        }  
+    if (rel == 'Manutenção Semanal') { TD.style.background = '#F3F3F3'; } 
+    if (rel == 'Grandes Paradas') { TD.style.background = '#FFF8E3'; } 
+    if (rel == 'Dias Uteis sem Contrato') { TD.style.background = '#FFF8E3C'; } 
+    if (rel == 'Horas Manuntenção') { TD.style.background = '#FFF8E3'; } 
+    if (rel == 'Outros') { TD.style.background = '#FFF8E3'; } }
 
+  
 
-
+    
