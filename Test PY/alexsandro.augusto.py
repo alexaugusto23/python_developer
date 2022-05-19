f1b7ec6388feb7c214290dab9366b0223597f91f
alexaugusto23 <contato.alexaugusto@hotmail.com>
2022-05-19 11:59:04 -0300

    \n

--- Test PY/alexsandro.augusto.py
+++ Test PY/alexsandro.augusto.py
@@ -20,6 +20,7 @@ NAME_FILE_JSON = 'jodi_gas_beta.json'
 ENCOD = 'ISO-8859-1'
 PATH_FILE = os.path.join(PATH, TARGET)
 PATH_FILE_JSON = os.path.join(PATH, NAME_FILE_JSON)
+MARK = 60
 
 # Functions
 def verify_dirictory(PATH, TARGET):
@@ -32,10 +33,10 @@ def verify_dirictory(PATH, TARGET):
                 path_file = os.path.join(dirpath, file)
                 try:
                     os.remove(path_file)
-                    print(f'- File Removed -')
+                    print(f'\n- File Removed -\n')
                     
                 except OSError as e:
-                    print(f"Error:{ e.strerror}")
+                    print(f"\nError:{ e.strerror}\n")
             #  print(file)    
 
 # Program
@@ -66,10 +67,10 @@ with ZipFile(PATH_FILE) as file_zip:
 
 
 print(f"\n")
-print(f"#" * 20)
-print(f"Shape: {_df.shape}")
-print(_df)
-print(f"#" * 20)
+print(f"#" * MARK)
+print(f"\nShape: {_df.shape}\n")
+print(f"\n_df}\n")
+print(f"#" * MARK)
 print(f"\n")
 
 dict_country = {'AD':'Andorra','AE':'Emirados Árabes Unidos','AF':'Afeganistão','AG':'Antígua e Barbuda','AI':'Anguilla','AL':'Albânia','AM':'Armênia','AN':'Antilhas Holandesas','AO':'Angola','AP':'Organização Regional de Propriedade Industrial Africana','AQ':'Antartica','AR':'Argentina','AS':'Samoa Americana','AT':'Áustria','AU':'Austrália','AW':'Aruba','AZ':'Azerbaidjão','BA':'Bósnia e Herzegóvina','BB':'Barbados','BD':'Bangladesh','BE':'Bélgica','BF':'Burkina Faso','BG':'Bulgária','BH':'Bareine','BI':'Burundi','BJ':'Benin','BM':'Bermudas','BN':'Brunei Darussalam','BO':'Bolívia','BR':'Brasil','BS':'Bahamas','BT':'Butão','BV':'Ilha Bouvet','BW':'Botswana','BX':'Benelux','BY':'Belarus','BZ':'Belize','CA':'Canadá','CC':'Ilhas Cocos','CD':'República Dem. Do Congo','CF':'República Centro Africana','CG':'Congo','CH':'Suíça','CI':'Costa do Marfim','CK':'Ilhas Cook','CL':'Chile','CM':'Camarões','CN':'China','CO':'Colômbia','CR':'Costa Rica','CU':'Cuba','CV':'Cabo Verde','CX':'Ilha Natal','CY':'Chipre','CZ':'República Tcheca','DE':'Alemanha','DJ':'Djibuti','DK':'Dinamarca','DM':'Dominica','DO':'República Dominicana','DZ':'Argélia','EC':'Equador','EE':'Estônia','EG':'Egito','EH':'Saara Ocidental','EM':'Escritório para Harmonização no Mercado Interno','ER':'Eritréia','ES':'Espanha','ET':'Etiópia','FI':'Finlândia','FJ':'Fiji','FK':'Ilhas Malvinas','FM':'Micronésia','FO':'Ilhas Faroe','FR':'França','GA':'Gabão','GB':'Reino Unido','GD':'Granada','GE':'Geórgia','GG':'Ilhas do Canal','GH':'Gana','GI':'Gibraltar','GL':'Groenlândia','GM':'Gambia','GN':'Guine','GP':'Guadalupe','GQ':'Guine Equatorial','GR':'Grécia','GS':'Geórgia do Sul e Ilhas Sandwich do Sul','GT':'Guatemala','GU':'Guam','GW':'Guiné Bissau','GY':'Guiana','HK':'Hong-Kong','HM':'Ilhas Heard e McDonald','HN':'Honduras','HR':'Croácia','HT':'Haiti','HU':'Hungria','ID':'Indonésia','ID':'Indonésia','IE':'Irlanda','IE':'Irlanda','IL':'Israel','IL':'Israel','IM':'Ilha do Homem','IN':'India','IO':'Territ.Britan.Oceano Índico','IQ':'Iraque','IQ':'Iraque','IR':'Irã','IR':'Irã','IS':'Islândia','IS':'Islândia','IT':'Itália','JE':'Jersey','JM':'Jamaica','JO':'Jordânia','JP':'Japão','KE':'Quênia','KG':'Quirguistão','KH':'Camboja','KI':'Kiribati','KM':'Comores','KN':'São Cristovão e Nevis','KP':'República Pop. Dem. da Coreia','KR':'República da Coréia','KW':'Kuwait','KY':'Ilhas Cayman','KZ':'Cazaquistão','LA':'Laos','LB':'Líbano','LC':'Santa Lúcia','LI':'Liechtenstein','LK':'Sri Lanka','LR':'Libéria','LS':'Lesoto','LT':'Lituânia','LU':'Luxemburgo','LV':'Letônia','LY':'Líbia','MA':'Marrocos','MC':'Mônaco','MD':'República da Moldova','ME':'Montenegro','MG':'Madagascar','MH':'Ilhas Marshall','MK':'República da Macedonia','ML':'Mali','MM':'Mianmá','MN':'Mongólia','MO':'Macau','MP':'Ilhas Marianas do Norte','MQ':'Martinica','MR':'Mauritânia','MS':'Mont Serrat','MT':'Malta','MU':'Maurício','MV':'Maldivas','MW':'Malawi','MX':'México','MY':'Malásia','MZ':'Moçambique','NA':'Namíbia','NC':'Nova Caledônia','NE':'Níger','NF':'Ilha Norfalk','NG':'Nigéria','NI':'Nicarágua','NL':'Holanda','NO':'Noruega','NP':'Nepal','NR':'Nauru','NZ':'Nova Zelândia','OA':'Organização Africana de Propriedade Intelectual (OAPI)','OM':'Omã','PA':'Panamá','PE':'Peru','PF':'Polinésia Francesa','PG':'Papua Nova Guiné','PH':'Filipinas','PK':'Paquistão','PL':'Polônia','PM':'Saint Pierre e Miquelon','PN':'Pitcairn','PR':'Porto Rico','PS':'Território Ocupado Palestino','PT':'Portugal','PW':'Palau','PY':'Paraguai','QA':'Catar','RE':'Reunião','RO':'Romênia','RS':'Sérvia','RU':'Federação Russa','RW':'Ruanda','SA':'Arábia Saudita','SB':'Ilhas Salomão','SC':'Seychelles','SD':'Sudão','SE':'Suécia','SG':'Singapura','SH':'Santa Helena','SI':'Eslovenia','SJ':'Svalbard e Jan Mayen','SK':'Eslováquia','SL':'Serra Leoa','SM':'São Marino','SN':'Senegal','SO':'Somália','SR':'Suriname','ST':'São Tomé e Príncipe','SV':'El Salvador','SY':'Síria','SZ':'Suazilândia','TC':'Ilhas Turks e Caicos','TD':'Chade','TF':'Terras Austrais Francesas','TG':'Togo','TH':'Tailândia','TJ':'Tadjiquistão','TK':'Tokelau','TL':'Timor-Leste','TM':'Turcomenistão','TN':'Tunísia','TO':'Tonga','TR':'Turquia','TT':'Trinidad e Tobago','TV':'Tuvalu','TW':'Taiwan','TZ':'República Unida da Tanzânia','UA':'Ucrânia','UG':'Uganda','UM':'Ilhas Menores','US':'Estados Unidos da América','UY':'Uruguai','UZ':'Uzbequistão','VA':'Vaticano','VC':'São Vicente e Granadinas','VE':'Venezuela','VG':'Ilhas Virgens (Britânicas)','VI':'Ilhas Virgens (U.S.)','VN':'Vietnã','VU':'Vanuatu','WF':'Ilhas Wallis e Futura','WO':'Organização Mundial de Propriedade Intelectual – OMPI (WIPO)','WS':'Samoa Ocidental','YE':'Iêmen','YU':'Yugoslávia','ZA':'Africa do Sul','ZM':'Zâmbia','ZR':'Zaire','ZW':'Zimbábue'}
@@ -94,9 +95,9 @@ for index, row in _df.iterrows():
     }})
 
 print(f"\n")
-print(f"#" * 20)
-print(_obj)
-print(f"#" * 20)
+print(f"#" * MARK)
+print(f"\n{_obj}\n")
+print(f"#" * MARK)
 print(f"\n")
 
 verify_dirictory(PATH, NAME_FILE_JSON)
