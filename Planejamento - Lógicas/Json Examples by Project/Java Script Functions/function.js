690bbbc9ae1828512cc9e2b28d1992dbc4f0988f
alexaugusto23 <contato.alexaugusto@hotmail.com>
2022-06-29 22:41:33 -0300

    forms ajuste 2906

--- Planejamento - Lógicas/Json Examples by Project/Java Script Functions/function.js
+++ Planejamento - Lógicas/Json Examples by Project/Java Script Functions/function.js
@@ -68,6 +68,20 @@ function funcc(hotInstance, TD, row, col, prop, value, cellProperties)
     if (rel == 'Horas Manuntenção') { TD.style.background = '#FFF8E3'; } 
     if (rel == 'Outros') { TD.style.background = '#FFF8E3'; } }
 
-  
+// Mudança no form 290622
+function funcc(hotInstance, TD, row, col, prop, value, cellProperties) 
+{   const dataRow = hotInstance.getSourceDataAtRow(row); 
+    const rel = dataRow.relatorio; 
+    if (dataRow.id >= 0) { cellProperties.readOnly = false; } 
+    if (rel == 'Marchas') { TD.style.background = '#F3F3F3'; TD.style.fontWeight = 'bold'; } 
+    if (rel == 'Horarios de Ponta') {
+        if (value > 0){TD.style.background = '#EAFB15'; TD.style.color = '#EAFB15';}
+        else{TD.style.background = '#F3F3F3';}
+        }  
+    if (rel == 'Manutenção Semanal') { TD.style.background = '#F3F3F3'; } 
+    if (rel == 'Grandes Paradas') { TD.style.background = '#FFF8E3'; } 
+    if (rel == 'Com ou Sem Contrato') { TD.style.background = '#FFF8E3C'; } 
+    if (rel == 'Horas Manuntenção') { TD.style.background = '#FFF8E3'; } 
+    if (rel == 'Outros') { TD.style.background = '#FFF8E3'; } } 
 
-    
+
