0f2a2cbcbf961342d2d81558cef47e7841562fe5
alexaugusto23 <contato.alexaugusto@hotmail.com>
2022-12-22 18:30:29 -0300

    apreendendo R

---
+++ R Scripts/comando_basicos.R
@@ -0,0 +1,19 @@
+# Aprender comando básicos R
+
+# Usar o R como calculadora.
+
+5 + 5
+5 - 2
+5 * 2
+8 / 4
+5 ^ 2
+25 ^ 0.5
+sqrt(25)
+(5+5) + (10-5)
+
+# Criando Objetos
+a=5
+b=6
+a * b
+
+# Criando Vetores
